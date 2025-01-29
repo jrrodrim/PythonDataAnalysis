74cb73dc3ad4eb68c5b8513f9b9da931027b578f
jrrodrim <42898155+jrrodrim@users.noreply.github.com>
2025-01-29 03:06:10 +0000

    Sesion5 27 de Enero de 2025

---
+++ Sesion5/data_pipeline/config.py
@@ -0,0 +1,21 @@
+#Database configure
+DATABASE_CONFIG= {
+    'host':'127.0.0.1',
+    'port': '3310',
+    'user':'root',
+    'password':'root',
+    'database':'retail_db'
+}
+
+CSV_FILES = {
+    'categories':'data/categories.csv',
+    'customers':'data/customers.csv',
+    'departments':'data/departments.csv',
+    'order_items':'data/order_items.csv',
+    'orders':'data/orders.csv',
+    'products':'data/products.csv'
+
+} 
+
+LOG_FILE='Logs/pipeline.log'
+
